f8221cca3e065f88346f8c0461d5b701d486ef0b
Akramay <akram.ayman007@gmail.com>
2024-05-04 22:37:36 +0300

    login and browse form
    changed in browse the picture to fill the card perfectly and in login form added that you don't have to click on login you can just hit enter on your keyboard

--- public/script/loginval.js
+++ public/script/loginval.js
@@ -32,6 +32,15 @@ else{
 
 
 
+const passwordInput = document.querySelector('#pass');
+passwordInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    const button = document.querySelector('input[type="button"]');
+    button.click();
+  }
+});
+
 
 
 
