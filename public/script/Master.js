f4dcfff2610d6b18b0dca98bcba392a4c6d6ad0f
mazenamir94 <113852215+mazenamir94@users.noreply.github.com>
2024-05-05 16:42:22 +0300

    master bnuttons making container

--- public/script/Master.js
+++ public/script/Master.js
@@ -21,4 +21,55 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 });
+// document.getElementById('popupBtn').addEventListener('click', function() {
+//   document.getElementById('popupContainer').style.display = 'block';
+// });
 
+// document.getElementById('closeBtn').addEventListener('click', function() {
+//   document.getElementById('popupContainer').style.display = 'none';
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+  let Admin = document.getElementById("Admin");
+  let container1 = document.getElementById("container1");
+  let User = document.getElementById("User");
+  let container2 = document.getElementById("container2");
+  let Store = document.getElementById("Store");
+  let container3 = document.getElementById("container3");
+  Admin.addEventListener("click", function() {
+    container1.style.display = "flex";
+    container2.style.display = "none";
+    container3.style.display = "none";
+  });
+  User.addEventListener("click", function() {
+         container2.style.display = "block";
+         container1.style.display = "none";
+         container3.style.display = "none";
+  });
+  Store.addEventListener("click", function() {
+    container3.style.display = "block";
+    container2.style.display = "none";
+    container1.style.display = "none";
+
+});
+
+
+});
+// document.addEventListener("DOMContentLoaded", function() {
+//   let showButton = document.getElementById("User");
+//   let container = document.getElementById("container2");
+
+//   showButton.addEventListener("click", function() {
+//     container.style.display = "block";
+  
+//   });
+// });
+// document.addEventListener("DOMContentLoaded", function() {
+//   let showButton = document.getElementById("Store");
+//   let container = document.getElementById("container3");
+
+//   showButton.addEventListener("click", function() {
+//     container.style.display = "block";
+  
+//   });
+// });
