918ee32e9cfaff4f77b1b45e276f0e2c130a2878
YoussefMa7moud <ymahmoud182@yahoo.com>
2024-05-05 18:52:44 +0300

    MyAccount page
    added html css and js files for the myaccount page which the user can update his info and also see his ongoing tickets and his past tickets also added animations to make it more user friendly

--- public/script/MyAccount.js
+++ public/script/MyAccount.js
@@ -1,7 +1,6 @@
 function logout() {
     if(localStorage.getItem('isLoggedIn') === 'true') {
         var logoutbtn = document.getElementById('logout');
-        logoutbtn.textContent = 'Log out';
         logoutbtn.onclick = function() {
             localStorage.setItem('isLoggedIn', 'false');
             window.location.href = 'index.html';
@@ -9,3 +8,46 @@ function logout() {
     }
 }
 window.onload = logout;
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let past = document.getElementById("past");
+    let container1 = document.getElementById("past-tickets");
+    let ongoing = document.getElementById("ongoing");
+    let container2 = document.getElementById("ongoing-tickets");
+    let info = document.getElementById("info");
+    let container3 = document.getElementById("my-info");
+
+    // Initially hide all containers
+    container1.style.display = "none";
+    container2.style.display = "none";
+    
+
+    past.addEventListener("click", function() {
+      container1.style.display = "block";
+      container2.style.display = "none";
+      container3.style.display = "none";
+    });
+
+    ongoing.addEventListener("click", function() {
+      container2.style.display = "block";
+      container1.style.display = "none";
+      container3.style.display = "none";
+    });
+
+    info.addEventListener("click", function() {
+      container3.style.display = "block";
+      container2.style.display = "none";
+      container1.style.display = "none";
+    });
+});
+
+
+
+
+
+
+
+
+
