d3b96eb6af9c085837723624a502ef64b92d9bcf
Akramay <akram.ayman007@gmail.com>
2024-06-13 21:06:51 +0300

    more cards
    added more cards with a button called load more to view them

--- public/script/Browse.js
+++ public/script/Browse.js
@@ -19,3 +19,30 @@ window.onload = function() {
       }
   };
 };
+
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 2;
+loadMoreBtn.addEventListener("click",function(){
+    let boxes = [...document.querySelectorAll('.content-wrapper .content .card')];
+    for (let i = currentItem; i < currentItem + 2; i++) { // Load one card at a time
+        boxes[i].style.display = 'grid';
+    }
+    currentItem += 2;
+
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+});
+
+// loadMoreBtn.onclick = () => {
+//     let boxes = [...document.querySelectorAll('.content-wrapper .content .card')];
+//     for (let i = currentItem; i < currentItem + 2; i++) { // Load one card at a time
+//         boxes[i].style.display = 'grid';
+//     }
+//     currentItem += 2;
+
+//     if (currentItem >= boxes.length) {
+//         loadMoreBtn.style.display = 'none';
+//     }
+// };
