124191feb14ee74dad401b0c9da4b0003be1a7d8
Omar400-cpu <omardarhous123456@gmail.com>
2024-06-17 19:01:39 +0300

    routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const DATALOG = require("./MODELS/loginDB.js");
 const TICKETS= require("./MODELS/ADDTickets.js")
+const ADroute=require("./Routes/Admin.js")
+const USroute=require("./Routes/User.js")
+const MSroute=require("./Routes/Master.js")
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
@@ -12,72 +15,77 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
+app.use("/Admin",ADroute);
+app.use("/index",USroute);
+app.use("/Master",MSroute);
+app.use(fileUpload()); 
+app.use(express.static(path.join(__dirname,'public'))); 
+app.use(session({ secret: credentials.Secret, resave: true, }));
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// app.get('/login', (req, res) => {
+//     res.render('login.ejs');
+//   });
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs');
-  });
+//   app.post('/login/addData', async (req, res) => {
+//     try {
+//         const loginDB = await DATALOG.create(req.body);
+//         res.status(200).json(loginDB);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// });
 
-  app.post('/login/addData', async (req, res) => {
-    try {
-        const loginDB = await DATALOG.create(req.body);
-        res.status(200).json(loginDB);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+//   app.get('/index', (req, res) => {
+//     res.render('index.ejs');
+//   });
 
-  app.get('/index', (req, res) => {
-    res.render('index.ejs');
-  });
+//   app.get('/Browse', (req, res) => {
+//     res.render('browse.ejs');
+//   });
 
-  app.get('/Browse', (req, res) => {
-    res.render('browse.ejs');
-  });
+//   app.get('/Store', (req, res) => {
+//     res.render('store.ejs');
+//   });
 
-  app.get('/Store', (req, res) => {
-    res.render('store.ejs');
-  });
+//   app.get('/BookNow', (req, res) => {
+//     res.render('BookNow.ejs');
+//   });
 
-  app.get('/BookNow', (req, res) => {
-    res.render('BookNow.ejs');
-  });
-
-  app.get('/Admin', (req, res) => {
-    res.render('Admin.ejs');
-  });
+//   app.get('/Admin', (req, res) => {
+//     res.render('Admin.ejs');
+//   });
 
 
   
 
-  app.post('/ADMIN/ADD-TICKETS', async (req, res) => {
-    try {
-        const ADDTickets = await TICKETS.create(req.body);
-        res.status(200).json(ADDTickets);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+//   app.post('/ADMIN/ADD-TICKETS', async (req, res) => {
+//     try {
+//         const ADDTickets = await TICKETS.create(req.body);
+//         res.status(200).json(ADDTickets);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// });
 
 
 
 
 
-  app.get('/Master', (req, res) => {
-    res.render('Master.ejs');
-  });
+//   app.get('/Master', (req, res) => {
+//     res.render('Master.ejs');
+//   });
 
 
-  app.get('/POTM', (req, res) => {
-    res.render('potm.ejs');
-  });
+//   app.get('/POTM', (req, res) => {
+//     res.render('potm.ejs');
+//   });
 
-  app.get('/MyAccount', (req, res) => {
-    res.render('MyAccount.ejs');
-  });
+//   app.get('/MyAccount', (req, res) => {
+//     res.render('MyAccount.ejs');
+//   });
 
 
 
@@ -86,7 +94,7 @@ app.listen(3000, () => {
 });
 
 
-mongoose.connect("mongodb+srv://youssef2207740:Pi1p2TXCiVVEIT39@user-data.blnfrn5.mongodb.net/USER-DATA?retryWrites=true&w=majority&appName=USER-DATA")
+mongoose.connect("mongodb+srv://youssef2207740:Pi1p2TXCiVVEIT39@user-data.blnfrn5.mongodb.net/USER-DATA?retryWrites=true&w=majority&appName=USER-DATA",{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   console.log('Mongo Connected!');
 })
