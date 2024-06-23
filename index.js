d501aaff91c91becae00c732698549b7d06bc819
YoussefMa7moud <ymahmoud182@yahoo.com>
2024-06-23 05:24:56 +0300

    verc2

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+const cors = require("cors");
+const corsConfig = {
+  origin: "*",
+  credential: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  };
+
+app.options("", cors (corsConfig));
+app.use(cors(corsConfig));
+
 const masterRoutes = require('./Routes/masterRoutes');
 const adminRoutes = require('./Routes/adminRoutes');
 const indexRoutes = require('./Routes/indexRoutes');
