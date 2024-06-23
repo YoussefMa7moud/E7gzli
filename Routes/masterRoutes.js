2b254f1132ccbe9e293d8bad2b8dbd42e58f9d33
Omar400-cpu <omardarhous123456@gmail.com>
2024-06-24 00:09:40 +0300

    Update masterRoutes.js

--- Routes/masterRoutes.js
+++ Routes/masterRoutes.js
@@ -13,9 +13,10 @@ const checkMaster = (req, res, next) => {
 
 router.get('/',controller.getMasters);
 router.post('/STORE', checkMaster, controller.STORE);
-router.post('/create-admin', checkMaster, controller.createAdmin);
+router.post('/Creat-admin', controller.createAdmin);
 router.post('/ADDPOTM', checkMaster, controller.ADDPOTM);
-router.delete('/delete-admin/:id', checkMaster, controller.deleteAdmin);
+router.delete('/delete-event/:id', controller.deleteAdmin);
+router.delete('/Delete-Admin/:id', controller.deleteAdmin);
 router.post('/Update-Activation', controller.activateuser);
 
 module.exports = router;
